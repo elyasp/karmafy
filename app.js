c452ee9cf78655af5a16e4a0f7f1c23ac8980f64
Tetsujin <elyasp@outlook.com>
2019-10-08 10:27:09 +0100

    update package.json

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(serveFavicon(join(__dirname, "public/images", "favicon.ico")));
-app.use(express.static(join(__dirname, "public")));
+app.use(serveFavicon(join(__dirname, "client/build", "favicon.ico")));
+app.use(express.static(join(__dirname, "/client/build")));
 
 // allow access to the API from different domains/origins
 app.use(
